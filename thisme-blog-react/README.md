4ce0f100b37bcd217f527f1a8a61aeeb4330096d
chenmo <2010557767@qq.com>
2019-09-23 17:04:23 +0800

    添加了部分文件和描述

---
+++ thisme-blog-react/README.md
@@ -0,0 +1,10 @@
+# thisme-blog-react 
+
+> 对[\<simple-react-app\>](../simple-react-app)的扩展, 在运行前保证你有```node``` 和```antd``` 
+
+#### Command
+
+* ```yarn start``` 启动服务默认端口8080
+* ```yarn build:dev``` 开发者模式构建
+* ```yarn build:pord``` 生产环境构建(需要改index.html的源)
+
---
+++ thisme-blog-react/src/editor/markdown-edit.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactMarkdown = require("react-markdown");
+var remark_toc_1 = require("remark-toc");
+var react_syntax_highlighter_1 = require("react-syntax-highlighter");
+var hljs_1 = require("react-syntax-highlighter/dist/esm/styles/hljs");
+require("../style/markdown.less");
+var useEffect = React.useEffect;
+/**
+ * 一个配置好的Markdown组件
+ * @author chenmo
+ * @param props
+ */
+var Markdown = function (props) {
+    return (React.createElement("div", null,
+        React.createElement(ReactMarkdown, { skipHtml: false, source: props.source, renderers: { code: exports.CodeBlack }, plugins: [remark_toc_1.default] })));
+};
+exports.default = Markdown;
+/**
+ * Markdown代码渲染器
+ * @constructor
+ */
+exports.CodeBlack = function (props) {
+    { /*<pre> 另一种实现方式
+        <code
+            ref={hljs.highlightBlock}
+            className={`language-${props.language}`}
+        >
+        {props.value}
+        </code>
+    </pre>*/
+    }
+    return (React.createElement(react_syntax_highlighter_1.default, { language: props.language, style: hljs_1.darcula }, props.value));
+};
+//# sourceMappingURL=markdown-edit.js.map
