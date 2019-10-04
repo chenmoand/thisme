ad329794ab97099ec67bc8bf4b1b76806b1fb7e9
chenmo <2010557767@qq.com>
2019-10-04 17:44:36 +0800

    增加一个gh-page

--- chenmo/src/test/java/com/brageast/chemo/TestMain.java
+++ chenmo/src/test/java/com/brageast/chemo/TestMain.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class TestMain {
-
     public static void main(String[] args) {
         final ExecutorService executorService = Executors.newWorkStealingPool();
     }
