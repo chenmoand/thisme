4911f905720188b3d2c909f3bcb773044117af69
chenmo <2010557767@qq.com>
2019-10-04 18:13:47 +0800

    增加一个build自动拷贝文件的配置

---
+++ simple-react-app/config/copyConfig.js
@@ -0,0 +1,16 @@
+// 这个是将static目录下的其他文件一并导入到build上
+const fs = require('fs');
+// const path = require('path');
+/**
+ * 不大会这个
+ */
+fs.readdir('./static', (err ,date) => {
+    if(err) console.log(err);
+    date.forEach(val => {
+        if(val !== 'index.html') {
+            fs.copyFile(`./static/${val}`, `./build/${val}`, err1 => {
+                if(err) console.log(err);
+            })
+        }
+    })
+});
