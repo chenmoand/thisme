e8c627366581e27959aa1dd6f429e78402f899ce
chenmo <2010557767@qq.com>
2019-10-21 17:54:01 +0800

    写了点java

---
+++ thisboot/src/main/java/com/brageast/blog/thisboot/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.brageast.blog.thisboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    /**
+     * web安全配置
+     * @param http
+     * @return
+     * https://docs.spring.io/spring-boot/docs/2.2.0.RELEASE/reference/htmlsingle/#boot-features-security-webflux
+     */
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        //TODO 暂时先放行全部
+        http.authorizeExchange()
+                .pathMatchers("/**")
+                .permitAll();
+        return http.build();
+    }
+}
