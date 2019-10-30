1d4494eb2303ea0d94a6e11bbc267617cecc6aaa
chenmo <2010557767@qq.com>
2019-10-30 09:53:12 +0800

    测试了一下mogodb

--- thisboot/src/main/java/com/brageast/blog/thisboot/config/MongodbConfig.java
+++ thisboot/src/main/java/com/brageast/blog/thisboot/config/MongodbConfig.java
@@ -1,11 +1,20 @@
 package com.brageast.blog.thisboot.config;
 
 
+import com.mongodb.MongoClientOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
 public class MongodbConfig {
 
+    @Bean
+    public MongoClientOptions mongoOptions() {
+        return MongoClientOptions
+                .builder()
+                .maxConnectionIdleTime(60000)
+                .build();
+    }
 }
--- thisboot/src/main/java/com/brageast/blog/thisboot/config/WebFluxConfig.java
+++ thisboot/src/main/java/com/brageast/blog/thisboot/config/WebFluxConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.ViewResolverRegistry;
@@ -34,6 +35,10 @@ public class WebFluxConfig implements WebFluxConfigurer {
     private IndexController indexController;
 
 
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().enableLoggingRequestDetails(true);
+    }
 
     @Bean
     public RouterFunction<ServerResponse> webFluxRoutesRegister() {
