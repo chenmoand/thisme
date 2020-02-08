9a02e580a5137749c70dd6cd1221c6bd595feec3
chenmo <2010557767@qq.com>
2020-02-08 21:13:51 +0800

    删除pom.xml多余的引入

--- thisboot/src/test/kotlin/com/brageast/blog/thisboot/ThisbootApplicationTests.kt
+++ thisboot/src/test/kotlin/com/brageast/blog/thisboot/ThisbootApplicationTests.kt
@@ -1,13 +1,8 @@
 package com.brageast.blog.thisboot
 
-import com.brageast.blog.thisboot.entity.RequestResult
-import com.brageast.blog.thisboot.entity.ResultState
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
-
-import reactor.kotlin.core.publisher.toMono
 
 @SpringBootTest
 class ThisbootApplicationTests {
