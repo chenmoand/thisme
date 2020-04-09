2154b6d462a036165eac7bc1234b67ffb602e3cc
chenmo <2010557767@qq.com>
2020-04-09 17:01:32 +0800

    人类迷惑提交

--- thisboot/src/main/kotlin/com/brageast/blog/thisboot/config/SecurityConfig.kt
+++ thisboot/src/main/kotlin/com/brageast/blog/thisboot/config/SecurityConfig.kt
@@ -2,11 +2,13 @@ package com.brageast.blog.thisboot.config
 
 import com.brageast.blog.thisboot.annotation.EnableAuthenticationManager
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 
+@Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @EnableAuthenticationManager
